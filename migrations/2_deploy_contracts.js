6132a216b50665c5cf1e7274aaf7b1cc470253dd
abdeljalil-asr <abdeljalil-asr@gmail.com>
2021-09-10 21:01:19 +0200

    creation of 3  smartcontract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,20 @@
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+
+
+module.exports = async function(deployer, network, accounts) {
+    //deploy Mock DAIT Token
+    await deployer.deploy(DaiToken)  
+    const daiToken = await DaiToken.deployed()
+
+    //deploy DappToken 
+    await deployer.deploy(DappToken)  
+    const dappToken = await DappToken.deployed()
+
+    //deploy tokenFarm 
+    await deployer.deploy(TokenFarm, dappToken.address , daiToken.address)  
+    const tokenFarm = await TokenFarm.deployed()
+
 };
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.5.0;
 contract TokenFarm {
 
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken){
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+
+    }
 
 
 
